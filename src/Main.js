e4bb32fff6ddf5b816c7e143f8cc9ced7ae3f779
samBrindle <brindlejsam@gmail.com>
2022-05-09 15:58:53 -0700

    added new files for my header, main, and footer

---
+++ src/Main.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Main extends React.Component {
+    render() {
+        return (
+            <main>
+                <article>
+                    <h2>Horned Beast</h2>
+                    <img src='' alt='' title=''></img>
+                    <p></p>
+                </article>
+            </main>
+        )
+    }
+}
+
+export default Main;
