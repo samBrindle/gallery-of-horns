fd01a201d4c786706404a5a38667f9830979e4fa
samBrindle <brindlejsam@gmail.com>
2022-05-10 17:27:49 -0700

    added bootstrap and other functionality

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,23 +1,50 @@
 import React from 'react';
-
-// const iterateArr = (arr) => {
-//     arr.forEach( (beast) => {
-//         <h2>{this.prop.title}</h2>
-//     })
-// }
+import Card from 'react-bootstrap/Card'
+import './HornedBeast.css';
 
 class HornedBeast extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            favs: 0,
+        }
+    }
+
+    favoriteImg = () => {
+        this.setState({
+            favs: this.state.favs + 1
+        })
+    }
+
     render () {
         return (
-            <article>
-                <h3>{this.props.title}</h3>
-                <img 
-                    src={this.props.imageURL}
-                    alt={this.props.title}
-                    title={this.props.title}
-                />
-                <p>{this.props.description}</p>
-            </article>
+
+        <Card style={{ width: '18rem' }} id='card'>
+        <Card.Body>
+            <Card.Title>{this.props.title}</Card.Title>
+            <Card.Img
+                src={this.props.imageURL}
+                alt={this.props.title}
+                title={this.props.title} >
+            </Card.Img>
+            <Card.Text>
+            {this.props.description}
+            </Card.Text>
+            <Card.Text className='favCounters' onClick={this.favoriteImg}> ❤️ {this.state.favs} Favorite this!</Card.Text>
+        </Card.Body>
+        </Card>
+
+            // <article>
+            //     <h3>{this.props.title}</h3>
+            //     <p>{this.state.favs} ❤️</p>
+            //     <p className='favoriteButton' onClick={this.favoriteImg}>Favorite this!</p>
+            //     <img 
+            //         src={this.props.imageURL}
+            //         alt={this.props.title}
+            //         title={this.props.title}
+            //     />
+            //     <p>{this.props.description}</p>
+            // </article>
         )
     }
 }
