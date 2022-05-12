85153fabbbbaa235755985126ac62e110717131e
samBrindle <brindlejsam@gmail.com>
2022-05-11 17:12:50 -0700

    added modal stuff!

--- src/Header.js
+++ src/Header.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import './Header.css';
 
 class Header extends React.Component {
     render() {
+
         return (
             <header>
                 <h1>Gallery of Horns</h1>
